bcc795563b16696eb9db8c05cee74af3c6618af8
munkucci <rodion.kolesnik@gmail.com>
2024-06-02 16:48:48 +0800

    улучшил формы

--- js/appMain.js
+++ js/appMain.js
@@ -1,23 +1,32 @@
+
 const inputs = document.querySelectorAll(".contact-input")
 const toggleBtn = document.querySelector(".theme-toggle")
+const allElements = document.querySelectorAll("*")
 
-toggleBtn.addEventListener("click", () =>{
-	document.body.classList.toggle("dark")
+toggleBtn.addEventListener("click", () => {
+	document.body.classList.toggle("dark");
+	allElements.forEach(el => {
+		el.classList.add("transition")
+		setTimeout(() => {
+			el.classList.remove("transition")
+		}, 1000)
+	})
 })
 
-inputs.forEach(ipt =>{
-	ipt.addEventListener("focus", () =>{
+inputs.forEach(ipt => {
+	ipt.addEventListener("focus",() => {
 		ipt.parentNode.classList.add("focus")
 		ipt.parentNode.classList.add("not-empty")
+
 	})
-	ipt.addEventListener("blur", () =>{
-		if(ipt.value == "") {
+	ipt.addEventListener("blur",() => {
+		if(ipt.value == ""){
 			ipt.parentNode.classList.remove("not-empty")
 		}
 		ipt.parentNode.classList.remove("focus")
-		
 	})
 })
+
 
 window.addEventListener('scroll', e => {
 	document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
